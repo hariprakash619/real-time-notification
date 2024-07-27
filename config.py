c875bffaa56be39fe026322be6ab3d2a988c2c77
hariprakash619 <hariprakash.619@gmail.com>
2024-07-27 15:55:59 -0700

    code

---
+++ config.py
@@ -0,0 +1,15 @@
+config = {
+    "google_api_key": "xxxxxxxxxxxxxxxxxx",
+    "youtube_playlist_id": "PLr3-bEi1N4khKGCtuPpO-QSSJlRLooJ2v",
+    "kafka": {
+        "bootstrap.servers": "xxxxxxx",
+        "security.protocol": "SASL_SSL",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "xxxxxx",
+        "sasl.password": "xxxxxxxxxxxx",
+    },
+    "schema_registry": {
+        "url": "https://xxxxxxx.aws.confluent.cloud",
+        "basic.auth.user.info": "xxxxxxxx",
+    }
+}
